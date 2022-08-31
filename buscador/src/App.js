0bce03366fdadece1b4567f43a802079ac85c254
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2022-08-31 13:13:52 -0300

    criação do buscador de CEP

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component }  from 'react';
+import {useState} from 'react';
+import {FiSearch} from 'react-icons/fi';
+import './buscador.css';
 
 function App() {
+
+  const [input, setInput] = useState('teste') //Input define o estado, e setInput define uma função passando um valor novo para ele.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+      <h2 className='subtitle'>Digite um CEP abaixo e busque informações</h2>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu CEP" value={input}></input>
+        <button className="buttonSearch"> 
+        <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 89400000</h2>
+        <span>Rua: Avenida João Pessoa</span>
+        <span>Complemento: Casa</span>
+        <span>São Pedro</span>
+        <span>Porto União - SC</span>
+      
+      </main>
+      
     </div>
   );
 }
