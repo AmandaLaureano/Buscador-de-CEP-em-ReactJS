3038e57de45964ec928f75070e29ddbde9d52b06
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2022-09-05 13:24:16 -0300

    up

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -2,10 +2,26 @@ import React, { Component }  from 'react';
 import {useState} from 'react';
 import {FiSearch} from 'react-icons/fi';
 import './buscador.css';
+import api from './services/api';
 
 function App() {
 
-  const [input, setInput] = useState('teste') //Input define o estado, e setInput define uma função passando um valor novo para ele.
+  const [input, setInput] = useState('') //Input define o estado, e setInput define uma função passando um valor novo para ele.
+
+  async function handleSearch(){
+   if(input === ""){
+    alert("Digite um CEP!")
+    return;
+   }
+
+   try{
+    const response = await api.get(`${input}/json`);
+    console.log(response)
+   }catch{
+    alert("Ops! Erro ao buscar o CEP digitado");
+    setInput("")
+   }
+  }
 
   return (
     <div className="container">
@@ -14,7 +30,7 @@ function App() {
 
       <div className="containerInput">
         <input type="text" placeholder="Digite seu CEP" value={input} onChange={(e) => setInput(e.target.value)}></input>
-        <button className="buttonSearch"> 
+        <button className="buttonSearch" onClick={handleSearch}> 
         <FiSearch size={25} color="#FFF"/>
         </button>
       </div>
